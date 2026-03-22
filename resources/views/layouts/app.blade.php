d8d6163b7de287e9489a53f1c8bebdd20c5a861c
Enmanuel Quintero <qenmanuel2@gmail.com>
2026-03-22 15:00:07 -0600

    server

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,6 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
 
-
 <style>
 
     .flex-grow-1 {
